56d8a33429b7bcd9545c79b9d8adeae015ee8acd
game-source <game-source@254f5b4c-bdf4-4503-907e-934c7b59e499>
2007-12-27 19:07:48 +0000

    [project @ bdb8ce45ecb5d8c7ebd6e3c3a2724547c6440403] Changed convey sessions -> tokens. For some reason it was using BRASS which should be BRONZE 03/09/07 Hal.
    Original author: w4dimenscor <w4dimenscor>
    Date: 2007-03-09 07:45:07+00:00
    
    git-svn-id: svn+ssh://4dimensions.org/home/game-source/repositories/gamesrc@335 254f5b4c-bdf4-4503-907e-934c7b59e499

--- mordecai.c
+++ mordecai.c
@@ -232,7 +232,7 @@ ACMD(do_convey) {
         if (GET_PRACTICES(ch) >= amount) {
             ch->Send("You convey %lld practice sessions to %lld brass tokens.\r\n",
                      amount, amount / 20);
-            GET_BRASS_TOKEN_COUNT(ch) += amount / 20;
+            GET_BRONZE_TOKEN_COUNT(ch) += amount / 20;
             GET_PRACTICES(ch) -= amount;
             convert_tokens(ch);
 
