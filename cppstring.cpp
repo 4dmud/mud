2f076c376e1747af9531a99de8b9628315e3f656
game-source <game-source@254f5b4c-bdf4-4503-907e-934c7b59e499>
2007-12-27 19:03:02 +0000

    [project @ 780691c14029dffb91cf6a218d7157e0cc13cec1] Added cppstring calls to convert char arrays into cstrings using a printf format
    Original author: w4dimenscor <w4dimenscor>
    Date: 2006-05-30 09:24:16+00:00
    
    git-svn-id: svn+ssh://4dimensions.org/home/game-source/repositories/gamesrc@194 254f5b4c-bdf4-4503-907e-934c7b59e499

---
+++ cppstring.cpp
@@ -0,0 +1,62 @@
+//
+// C++ Implementation: string
+//
+// Description: 
+//
+//
+// Author: Jamie Nelson <mordecai@xtra.co.nz>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "conf.h"
+#include "sysdep.h"
+string vformat(const char *fmt, va_list argPtr);
+
+
+string format(const char* fmt ...) {
+  va_list argList;
+  va_start(argList, fmt);
+  string result = vformat(fmt, argList);
+  va_end(argList);
+  
+  return result;
+}
+
+
+string vformat(const char *fmt, va_list argPtr) {
+    // We draw the line at a 1MB string.
+  const int maxSize = 1000000;
+  
+    // If the string is less than 161 characters,
+    // allocate it on the stack because this saves
+    // the malloc/free time.
+  const int bufSize = 161;
+  char stackBuffer[bufSize];
+  
+  int attemptedSize = bufSize - 1;
+  
+  int numChars = vsnprintf(stackBuffer, attemptedSize, fmt, argPtr);
+  
+  if (numChars >= 0) {
+        // Got it on the first try.
+    return string(stackBuffer);
+  }
+  
+    // Now use the heap.
+  char* heapBuffer = NULL;
+  
+  while ((numChars == -1) && (attemptedSize < maxSize)) {
+        // Try a bigger size
+    attemptedSize *= 2;
+    heapBuffer = (char*)realloc(heapBuffer, attemptedSize + 1);
+    numChars = vsnprintf(heapBuffer, attemptedSize, fmt, argPtr);
+  }
+  
+  string result = string(heapBuffer);
+  
+  free(heapBuffer);
+  
+  return result;
+  
+}
