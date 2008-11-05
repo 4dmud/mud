262d6e2d8d08772d982fce1f1000d7280dce0936
mordecai <mordecai@254f5b4c-bdf4-4503-907e-934c7b59e499>
2008-11-05 08:27:19 +0000

    Fixed issue where keys would still turn to dust even when  the keystay flag was on it.
    git-svn-id: svn+ssh://4dimensions.org/home/game-source/repositories/gamesrc@620 254f5b4c-bdf4-4503-907e-934c7b59e499

--- objsave.c
+++ objsave.c
@@ -737,8 +737,10 @@ int Crash_is_unrentable(struct obj_data *obj)
 {
   if (!obj)
     return 0;
+  if (GET_OBJ_TYPE(obj) == ITEM_KEY && OBJ_FLAGGED(obj, ITEM_KEYSTAY))
+    return 0;
 
-  if (IS_OBJ_STAT(obj, ITEM_NORENT) || (GET_OBJ_TYPE(obj) == ITEM_KEY && !OBJ_FLAGGED(obj, ITEM_KEYSTAY)))
+  if (IS_OBJ_STAT(obj, ITEM_NORENT))
     return 1;
 
   return 0;
