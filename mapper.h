ae41ddd33a0fc5e866457c85939175089c033f2b
prometheus <prometheus@254f5b4c-bdf4-4503-907e-934c7b59e499>
2011-09-18 07:27:08 +0000

    Fixed the line spacing. And I changed "/"" to "|" to see if it shows up in the map command Prom
    git-svn-id: svn+ssh://4dimensions.org/home/game-source/repositories/gamesrc@1024 254f5b4c-bdf4-4503-907e-934c7b59e499

--- mapper.h
+++ mapper.h
@@ -23,39 +23,41 @@ void  draw_map (Character *ch);
 char *get_exits(Character *ch);
 char *msdp_map (Character *ch);
 
+// Changing "/"" to "|" for relevant Sectors --> Prom
+
 static struct map_bit_data {
     char bit[2];
     char colour[5];
     char name[15];
     } map_bit[] = {
-{"%", "{cW", "Inside"    },//SECT_INSIDE         
-{"#", "{cW", "City"      },//SECT_CITY           
-{"\"","{cG", "Field"     },//SECT_FIELD          
-{"@", "{cg", "Forest"    },//SECT_FOREST         
-{"^", "{cG", "Hills"     },//SECT_HILLS          
-{"^", "{cy", "Mountain"  },//SECT_MOUNTAIN       
-{":", "{cC", "Water-Swim"},//SECT_WATER_SWIM     
-{":", "{cB", "Water-Boat"},//SECT_WATER_NOSWIM   
-{"~", "{cb", "Underwater"},//SECT_UNDERWATER	    
-{"%", "{cC", "Flying"    },//SECT_FLYING         
-{"~", "{cY", "Desert"    },//SECT_DESERT         
-{".", "{cW", "Space"     },//SECT_SPACE	
-{"-", "{cY", "Road"      },//SECT_ROAD	
-{"@", "{cY", "Entrance"  },//SECT_ENTRANCE	
-{"@", "{cC", "Atmosphere"},//SECT_ATMOSPHERE 
-{"*", "{cY", "Sun"       },//SECT_SUN	
-{"O", "{cL", "Blackhole" },//SECT_BLACKHOLE	
-{"<", "{cM", "Vehicle"   },//SECT_VEHICLE	
-{"\"", "{cg", "Swamp"     },//SECT_SWAMP
-{";", "{cM", "Reef"      },//SECT_REEF  
-{"\"", "{cW", "Tundra"    },//SECT_TUNDRA
-{"o", "{cW", "Snow"      },//SECT_SNOW
-{":", "{cC", "Ice"       },//SECT_ICE
-{"\"", "{cy", "Praire"     },//SECT_PRAIRIE
-{"'", "{cr", "Badlands"       },//SECT_BADLANDS
-{"+", "{cy", "Rail"       },//SECT_RAIL
+{"%", "{cW", "Inside"      },//SECT_INSIDE         
+{"#", "{cW", "City"        },//SECT_CITY           
+{"|", "{cG", "Field"       },//SECT_FIELD          
+{"@", "{cg", "Forest"      },//SECT_FOREST         
+{"^", "{cG", "Hills"       },//SECT_HILLS          
+{"^", "{cy", "Mountain"    },//SECT_MOUNTAIN       
+{":", "{cC", "Water-Swim"  },//SECT_WATER_SWIM     
+{":", "{cB", "Water-Boat"  },//SECT_WATER_NOSWIM   
+{"~", "{cb", "Underwater"  },//SECT_UNDERWATER	    
+{"%", "{cC", "Flying"      },//SECT_FLYING         
+{"~", "{cY", "Desert"      },//SECT_DESERT         
+{".", "{cW", "Space"       },//SECT_SPACE	
+{"-", "{cY", "Road"        },//SECT_ROAD	
+{"@", "{cY", "Entrance"    },//SECT_ENTRANCE	
+{"@", "{cC", "Atmosphere"  },//SECT_ATMOSPHERE 
+{"*", "{cY", "Sun"         },//SECT_SUN	
+{"O", "{cL", "Blackhole"   },//SECT_BLACKHOLE	
+{"<", "{cM", "Vehicle"     },//SECT_VEHICLE	
+{"|", "{cg", "Swamp"       },//SECT_SWAMP
+{";", "{cM", "Reef"        },//SECT_REEF  
+{"|", "{cW", "Tundra"      },//SECT_TUNDRA
+{"o", "{cW", "Snow"        },//SECT_SNOW
+{":", "{cC", "Ice"         },//SECT_ICE
+{"|", "{cy", "Praire"      },//SECT_PRAIRIE
+{"'", "{cr", "Badlands"    },//SECT_BADLANDS
+{"+", "{cy", "Rail"        },//SECT_RAIL
 
-{"X", "{cx", "Unknown"   } //NUM_ROOM_SECTORS
+{"X", "{cx", "Unknown"     } //NUM_ROOM_SECTORS
     };
 
 
