639fe1bbb720de9f01c37f282ff78d3f304865a6
once <once@254f5b4c-bdf4-4503-907e-934c7b59e499>
2012-02-21 05:32:27 +0000

    More of the same.
    git-svn-id: svn+ssh://4dimensions.org/home/game-source/repositories/gamesrc@1091 254f5b4c-bdf4-4503-907e-934c7b59e499

--- fight.cpp
+++ fight.cpp
@@ -5340,7 +5340,7 @@ void raw_kill ( Character *ch, Character *killer )
 
           /* Now lets check percentage chance for deed to load */
           /* Percentage chance increases every 30 minutes      */
-          ct = (time(0) - killer->player.deeds.time_in)/300.00;
+          ct = (time(0) - killer->player.deeds.time_in)/150.00;
           ct += 2;     // Always 2 percentage chance anyways
           if (number(0, 100) < (int)ct) {
               obj = read_object(7, VIRTUAL);
