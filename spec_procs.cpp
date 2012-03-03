f2cd3e81367c9d1a5a96567cfcf85141ae185629
once <once@254f5b4c-bdf4-4503-907e-934c7b59e499>
2012-03-03 05:32:57 +0000

    Typo fix for deed rewards.
    git-svn-id: svn+ssh://4dimensions.org/home/game-source/repositories/gamesrc@1123 254f5b4c-bdf4-4503-907e-934c7b59e499

--- spec_procs.cpp
+++ spec_procs.cpp
@@ -588,7 +588,7 @@ SPECIAL(clan_deeds)
   }
   if (GET_DEED_COUNT(ch) == 100) {
   ch->Send("{{ccYou gain {cC24322978{cc exp and {cC5,000,000{cc gold for reaching the milestone of 100 deeds!\r\n");
-  send_to_all("{cY%s just reaached the deed milestone of 100 deeds!\r\n{cn", GET_NAME(ch));
+  send_to_all("{cY%s just reached the deed milestone of 100 deeds!\r\n{cn", GET_NAME(ch));
   gain_exp(ch, 24322978);
   GET_GOLD(ch) += 5000000;
   }
