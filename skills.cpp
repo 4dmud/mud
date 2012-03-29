b32b9a01abc5cb920a1751f797bc274434882770
prometheus <prometheus@254f5b4c-bdf4-4503-907e-934c7b59e499>
2012-03-29 02:37:09 +0000

    Removed the attack message in the code since I know about the attack messages file now Prom
    git-svn-id: svn+ssh://4dimensions.org/home/game-source/repositories/gamesrc@1133 254f5b4c-bdf4-4503-907e-934c7b59e499

--- skills.cpp
+++ skills.cpp
@@ -2196,11 +2196,6 @@ ASKILL ( skill_thrust )
                 *ch << "You are far too exhausted!";
                 return 0;
         }
-	// Attack message for thrust --> Prom
-	*ch << "You twirl your weapon and thrusts it into your enemy.\r\n";
-	// Messages to room and others (not pc) -- Prom
-	//act ("$e staggers as $n's weapon thrusts into it.", FALSE, ch, 0, NULL, TO_NOTVICT);
-        //
 	/* Only appropriately skilled PCs and uncharmed mobs */
         corpse_mod = 1;
 	skill_attack ( ch, vict, SKILL_THRUST, ( IS_NPC ( ch ) ? GET_LEVEL ( ch ) : total_chance ( ch, SKILL_THRUST ) ) > number ( 0, 120 ) );
