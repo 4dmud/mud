54f12692c9862f518f2a1898035770c8d70d0b68
thotter <thotter@254f5b4c-bdf4-4503-907e-934c7b59e499>
2012-04-25 20:56:11 +0000

    lisp: forgot to check in header file..
    git-svn-id: svn+ssh://4dimensions.org/home/game-source/repositories/gamesrc@1149 254f5b4c-bdf4-4503-907e-934c7b59e499

---
+++ lisp-internal.h
@@ -0,0 +1,14 @@
+#include "config.h"
+#include "sysdep.h"
+
+#include "structs.h"
+#include "utils.h"
+#include "db.h"
+#include "clan.h"
+
+extern vector <Room *> world_vnum;
+extern zone_rnum top_of_zone_table;
+extern vector <Zone> zone_table;
+
+extern vector <clan_rec> clan;
+extern int num_of_clans;
